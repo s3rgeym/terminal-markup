a324fb9feb6f37c2d45dfce3fbfcb33140692582
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2024-01-03 03:59:26 +0300

    отключение стилей

--- terminal_markup/escape_codes.py
+++ terminal_markup/escape_codes.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 
-import string
 from dataclasses import KW_ONLY, MISSING, Field, dataclass, fields
-from enum import Enum
-from typing import ClassVar, Iterable, NamedTuple
+from typing import ClassVar, Iterable, NewType
 
 from .utils.color import RGB
 from .utils.enum import CaseInsensetiveEnum
@@ -34,27 +32,30 @@ TextStyle = EscapeCode(
     start=1,
 )
 
+NotSet = NewType("NotSet", object)
+
 
 @dataclass
 class EscapeSequence:
     _: KW_ONLY
-    bold: bool = False
-    dim: bool = False
-    italic: bool = False
-    underline: bool = False
-    color: str | RGB | None = None
-    background: str | RGB | None = None
+    bold: bool | NotSet = NotSet
+    dim: bool | NotSet = NotSet
+    italic: bool | NotSet = NotSet
+    underline: bool | NotSet = NotSet
+    color: str | RGB | NotSet = NotSet
+    background: str | RGB | NotSet = NotSet
     CSI: ClassVar[str] = "\x1b["
 
     def gen_sequence(self) -> Iterable[int]:
         for x in ["bold", "dim", "italic", "underline"]:
-            if getattr(self, x):
+            val = getattr(self, x)
+            if val is not NotSet and val:
                 yield TextStyle(x).value
         for color, color_offset in [
             (self.color, 30),
             (self.background, 40),
         ]:
-            if color is None:
+            if color is NotSet:
                 continue
             if isinstance(color, str):
                 color = RGB.from_hex(color) or color
@@ -82,11 +83,11 @@ class EscapeSequence:
             # Нужно ли?
             if field.name not in other.__dict__:
                 continue
-            if field.default is not MISSING:
-                default_value = field.default
-            else:
-                default_value = field.default_factory()
-            if other.__dict__[field.name] != default_value:
+            # if field.default is not MISSING:
+            #     default_value = field.default
+            # else:
+            #     default_value = field.default_factory()
+            if other.__dict__[field.name] is not NotSet:
                 d[field.name] = other.__dict__[field.name]
         return EscapeSequence(**d)
 
